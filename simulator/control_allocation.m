d63f890ecf051f6e74da737ef78e7017d5466cb0
Azarakhsh Keipour <keipour@gmail.com>
2020-07-03 15:06:02 -0400

    Updated the license information in file headers

--- simulator/control_allocation.m
+++ simulator/control_allocation.m
@@ -2,7 +2,8 @@
 % Copyright (c) 2020 Carnegie Mellon University
 % This tool has been developed for educational purposes only as a 
 % control tutorial in Air Lab Summer School 2020 (https://theairlab.org). 
-% Azarakhsh Keipour (keipour [at] cmu.edu)
+% For License information please see the LICENSE file in the root directory.
+% Author: Azarakhsh Keipour (keipour [at] cmu.edu)
 % Please contact us for any questions or issues.
 %% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 
--- simulator/multirotor.m
+++ simulator/multirotor.m
@@ -1,8 +1,9 @@
-%% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+%%% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 % Copyright (c) 2020 Carnegie Mellon University
 % This tool has been developed for educational purposes only as a 
 % control tutorial in Air Lab Summer School 2020 (https://theairlab.org). 
-% Azarakhsh Keipour (keipour [at] cmu.edu)
+% For License information please see the LICENSE file in the root directory.
+% Author: Azarakhsh Keipour (keipour [at] cmu.edu)
 % Please contact us for any questions or issues.
 %% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 
