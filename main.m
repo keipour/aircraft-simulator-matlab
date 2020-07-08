3e5efe0c31fae24d7d00bb13ca472abfb2f17cbc
Azarakhsh Keipour <keipour@gmail.com>
2020-07-07 23:45:11 -0400

    Main file modified for the CTI project

--- main.m
+++ main.m
@@ -3,32 +3,40 @@ close all
 
 addpath('simulator');
 
-RotorPlacementAngles = [30, 90, 150, 210, 270, 330];
-RotorRotationDirections = [-1, 1, -1, 1, -1, 1];
+
+RotorPlacementAngles = [45, 135, 225, 315];
+RotorRotationDirections = [-1, 1, -1, 1];
 RotorDihedralAngle = 0;
-RotorSidewardAngle = [-30, 30, -30, 30, -30, 30];
+RotorSidewardAngle = 0;
 RotorInwardAngle = 0;
+
+% RotorPlacementAngles = [30, 90, 150, 210, 270, 330];
+% RotorRotationDirections = [-1, 1, -1, 1, -1, 1];
+% RotorDihedralAngle = 0;
+% RotorSidewardAngle = [-30, 30, -30, 30, -30, 30];
+% RotorInwardAngle = 0;
 
 m = multirotor(RotorPlacementAngles, RotorRotationDirections);
 m.SetRotorAngles(RotorInwardAngle, RotorSidewardAngle, RotorDihedralAngle);
+%m.Visualize();
+%m.VisualizeAxes();
+%m.AnalyzeDynamicManipulability(2, 2);
+
+c = controller(m);
+sim = simulation(m, c);
+
+sim.Controller.AttitudeController.SetPID(10, 0.3, 10);
+
 pos = [0; 0; 0];
 vel = [0; 0; 0];
 rpy = [0; 0; 0];
 omega = [0; 0; 0];
+sim.Multirotor.SetInitialState(pos, vel, rpy, omega);
+sim.TotalTime = 10;
+sim.SimulateAttitudeResponse([-10; 0; 30], true);
 
-c = controller(m);
+sim.Controller.PositionController.SetPID(5, 0.3, 5);
+%figure; 
+%sim.SimulatePositionResponse([100; 100; -100], 100, true);
 
-tic
-sim = simulation(m, c);
-
-m.AnalyzeAccelerationManipulability(2);
-
-% sim.Controller.AttitudeController.SetPID(5, 0, 5);
-% sim.Multirotor.SetInitialState(pos, vel, rpy, omega);
-% sim.TotalTime = 10;
-% %sim.SimulateAttitudeResponse([10; -10; 50], true);
-% sim.SimulatePositionResponse([1; 1; 1], 10, true);
-% toc
-% 
-% t = sim.GetTimeSteps();
-% graphics.PlotSignalsByName(sim, 3, {'pos', 'accel', 'rpy', 'rpy dot', 'rpm'}, true);
+graphics.PlotSignalsByName(sim, 3, {'pos', 'accel', 'rpy', 'rpy dot', 'sat'}, true);
