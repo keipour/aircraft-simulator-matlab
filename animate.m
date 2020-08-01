ae613d350e6e35913d6f5c47135f9787cf777fdd
Azarakhsh Keipour <keipour@gmail.com>
2020-08-01 10:03:29 -0400

    Animate: Added animation using the multirotor visualizer + hot keys
    - Hot keys for speeding up/down the animation
    - Hot keys for zooming in/out

--- animate.m
+++ animate.m
@@ -1,12 +1,17 @@
-function animate(multirotor, zoom_level)
+function animate(multirotor, zoom_level, speed)
     if nargin < 2
-        zoom_level = 0;
+        zoom_level = 2;
+    end
+    if nargin < 3
+        speed = 1;
     end
     
     num_of_zoom_levels = 5;
     zoom_level = min(zoom_level, num_of_zoom_levels);
     zoom_level = max(zoom_level, 0);
     min_zoom = 2; % in meters
+    
+    n_rotors = multirotor.NumOfRotors;
     
     % Load the data from the logger
 
@@ -21,8 +26,8 @@ function animate(multirotor, zoom_level)
 
     % Initialization
     
-    rotor_pos = zeros([3, 6]);
-    for i = 1 : 6
+    rotor_pos = zeros([3, n_rotors]);
+    for i = 1 : n_rotors
         arm_length = multirotor.Rotors{i}.ArmLength;
         dihedral = multirotor.Rotors{i}.DihedralAngle;
         arm_angle = multirotor.Rotors{i}.ArmAngle;
@@ -31,12 +36,20 @@ function animate(multirotor, zoom_level)
         rotor_pos(3,i) = arm_length * sind(dihedral);
     end
 
-    dir_pos = [multirotor.Rotors{1}.ArmLength * cosd(multirotor.Rotors{1}.DihedralAngle) / 2; 0; 0];
-
     % set up first frame
     close all; 
-    figH = figure('KeyPressFcn',@Key_Down);
-
+    fig = figure('KeyPressFcn',@Key_Down);
+    %fig = figure;%('visible','off');
+    graphics.PlotMultirotor(multirotor);
+    
+    dataObjs = findobj(fig,'-property','XData');
+    myplot = cell(length(dataObjs), 3);
+    for i = 1 : length(dataObjs)
+        myplot{i, 1} = dataObjs(i).XData;
+        myplot{i, 2} = dataObjs(i).YData;
+        myplot{i, 3} = dataObjs(i).ZData;
+    end
+    
     ind = 1;
     is_paused = false;
     while ind <= length(t)
@@ -49,15 +62,33 @@ function animate(multirotor, zoom_level)
         tic;
         
         % Exit the animation if the window is closed
-        if ~ishghandle(figH)
+        if ~ishghandle(fig)
             break
         end
         
-        draw_hex([x(ind), y(ind), z(ind)], [yaw(ind), pitch(ind), roll(ind)], ...
-            rotor_pos, dir_pos);
+        T = [eul2rotm([yaw(ind); pitch(ind); roll(ind)]', 'ZYX'), ...
+            [x(ind); y(ind); z(ind)]; 0, 0, 0, 1];
+        figure(fig);
+        for i = 1 : length(dataObjs)
+            if startsWith(dataObjs(i).Type, 'p')
+                data = [myplot{i, 1}, myplot{i, 2}, myplot{i, 3}]';
+                data = T * [data; ones(1, length(myplot{i, 1}))];
+                set(dataObjs(i),'XData', data(1, :)', 'YData', data(2, :)', 'ZData', data(3, :)');
+            elseif startsWith(dataObjs(i).Type, 'l')
+                data = [myplot{i, 1}; myplot{i, 2}; myplot{i, 3}];
+                data = T * [data; ones(1, length(myplot{i, 1}))];
+                set(dataObjs(i),'XData', data(1, :), 'YData', data(2, :), 'ZData', data(3, :));
+            end
+        end
+        
+        
+%    dir_pos = [multirotor.Rotors{1}.ArmLength * cosd(multirotor.Rotors{1}.DihedralAngle) / 2; 0; 0];
+%         figure(fig);
+%         draw_hex([x(ind), y(ind), z(ind)], [yaw(ind), pitch(ind), roll(ind)], ...
+%             rotor_pos, dir_pos);
 
         % Convert the coordinates to NED
-        set(gca, 'ydir', 'reverse');
+        set(fig.CurrentAxes, 'ydir', 'reverse');
     %    set(gca, 'zdir', 'reverse');
 
         xlabel('N');     ylabel('E');     zlabel('D');
@@ -69,8 +100,8 @@ function animate(multirotor, zoom_level)
         ylim(ylimits);
         zlim(zlimits);
 
-        strtitle = sprintf('Time: %4.1f   X: %4.1f   Y: %4.1f   Z: %4.1f   Roll: %4.1f   Pitch: %4.1f   Yaw: %4.1f', ...
-            t(ind), x(ind), y(ind), z(ind), rad2deg(roll(ind)), rad2deg(pitch(ind)), rad2deg(yaw(ind))); 
+        strtitle = sprintf('T: %4.1f  XYZ: (%4.1f, %4.1f, %4.1f)  RPY: (%4.1f, %4.1f, %4.1f)  ZM: %d  SPD: %4.2f', ...
+            t(ind), x(ind), y(ind), z(ind), rad2deg(roll(ind)), rad2deg(pitch(ind)), rad2deg(yaw(ind)), zoom_level, speed); 
 
         title(strtitle);
 
@@ -79,25 +110,29 @@ function animate(multirotor, zoom_level)
         exec_time = toc;
 
         current_time = t(ind);
-        while ind < length(t) && current_time + exec_time >= t(ind)
+        while ind < length(t) && current_time + exec_time * speed >= t(ind)
             ind = ind + 1;
             exec_time = toc;
         end
 
-        pause(t(ind) - current_time - exec_time);
+        pause(t(ind) - current_time - exec_time * speed);
         if ind == length(t)
             break;
         end
     end
     
     function Key_Down(src,event)
-        key_code = int8(event.Character);
-        if key_code == 32
+        key_code = int8(event.Character)
+        if key_code == 32 % space key
             is_paused = ~is_paused;
-        elseif key_code == 43 || key_code == 61
+        elseif key_code == 43 || key_code == 61 % + key
             zoom_level = min(zoom_level + 1, num_of_zoom_levels);
-        elseif key_code == 45
+        elseif key_code == 45 % - key
             zoom_level = max(zoom_level - 1, 0);
+        elseif key_code == 30 % up key
+            speed = speed * 2;
+        elseif key_code == 31 % down key
+            speed = speed / 2;
         end
     end
 end
@@ -135,7 +170,7 @@ function lim = calc_axis_limits(n_levels, level, r_x, minx, maxx, min_lim)
     end
     lim = [r_x - step / 2, r_x + step / 2];
     if lim(1) < minx
-        lim = [minx, step];
+        lim = [minx, minx + step];
     end
     if lim(2) > maxx
         lim = [maxx - step, maxx];
