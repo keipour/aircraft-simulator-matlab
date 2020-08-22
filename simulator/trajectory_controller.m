dc630007348d48488c15e83ca8596b4a5cb4c5d1
Azarakhsh Keipour <keipour@gmail.com>
2020-08-21 21:19:31 -0400

    Trajectory Ctrl: Sending the last waypoint after the sequence is finished

--- simulator/trajectory_controller.m
+++ simulator/trajectory_controller.m
@@ -7,6 +7,9 @@ classdef trajectory_controller < handle
     
     properties (SetAccess = private, GetAccess = private)
         Initialized = false;
+        Finished = false;
+        Waypoints
+        LastWaypoint
     end
     
     methods
@@ -22,13 +25,35 @@ classdef trajectory_controller < handle
                 'UAVType', 'multirotor', 'Waypoints', poses, ...
                 'YawAngles', yaws, 'TransitionRadius', radius);
             obj.LookAheadDistance = lookahead_dist;
+            obj.Waypoints = waypoints;
+            obj.LastWaypoint = waypoints(size(waypoints, 1), :)';
             obj.Initialized = true;
         end
         
         function [lookahead_pose, finished] = CalcLookaheadPoint(obj, pose)
+
+            % Return the last waypoint if the sequense is finished
+            if obj.Finished
+                lookahead_pose = obj.LastWaypoint;
+                finished = true;
+                return;
+            end
+                
+            % Calculate the lookahead point
             pose = [pose(1 : 3); deg2rad(pose(4))];
+
             [lookahead_point, ~, desired_yaw, ~, finished] = obj.WaypointFollower(pose, obj.LookAheadDistance);
-            lookahead_pose = [lookahead_point; rad2deg(desired_yaw)];
+
+            % Check if the sequense is finished and we need to send the
+            % last waypoint instead of the lookahead
+            if finished > 0
+                obj.Finished = true;
+                lookahead_pose = obj.LastWaypoint;
+            else
+                % Otherwise send the lookahead
+                lookahead_pose = [lookahead_point; rad2deg(desired_yaw)];
+            end
+            
         end
     end
 end
