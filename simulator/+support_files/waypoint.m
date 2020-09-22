0a5c3f09f2cacc27b2ce7690db02ae6562460ba8
Azarakhsh Keipour <keipour@gmail.com>
2020-09-22 18:26:34 -0400

    Waypoint: Added functions HasRPY and HasForce

--- simulator/+support_files/waypoint.m
+++ simulator/+support_files/waypoint.m
@@ -1,31 +1,32 @@
 classdef waypoint < handle
     
     properties
-        Type
         Position
         RPY
         Force
     end
     
-    properties (Constant)
-        Types_PositionYaw = 0;
-        Types_PositionRPY = 1;
-        Types_PositionYawForce = 2;
-        Types_PositionRPYForce = 3;
-    end
-    
     methods
-        function obj = waypoint(input, pos, rpy, force)
+        function obj = waypoint(input, rpy, force)
             if nargin == 1
-                obj = support_files.waypoint.ProcessWaypoints(input);
+                wp = support_files.waypoint.ProcessWaypoints(input);
+                if length(wp) > 1
+                    error('Only one waypoint can be initialized.');
+                end
             else
-                obj.Type = input;
-                obj.Position = pos;
+                obj.Position = input;
                 obj.RPY = rpy;
                 obj.Force = force;
             end
         end
         
+        function flag = HasRPY(obj)
+            flag = any(isnan(obj.RPY));
+        end
+        
+        function flag = HasForce(obj)
+            flag = any(isnan(obj.Force));
+        end
     end
     
     methods (Static)
@@ -47,17 +48,13 @@ classdef waypoint < handle
                     w = [];
                     switch size(input{i}, 2)
                         case 4
-                            w = support_files.waypoint(support_files.waypoint.Types_PositionYaw, ...
-                                input{i}(j, 1 : 3)', [NaN; NaN; input{i}(j, 4)], NaN(3, 1));
+                            w = support_files.waypoint(input{i}(j, 1 : 3)', [NaN; NaN; input{i}(j, 4)], NaN(3, 1));
                         case 6
-                            w = support_files.waypoint(support_files.waypoint.Types_PositionRPY, ...
-                                input{i}(j, 1 : 3)', input{i}(j, 4 : 6)', NaN(3, 1));
+                            w = support_files.waypoint(input{i}(j, 1 : 3)', input{i}(j, 4 : 6)', NaN(3, 1));
                         case 7
-                            w = support_files.waypoint(support_files.waypoint.Types_PositionYawForce, ...
-                                input{i}(j, 1 : 3)', [NaN; NaN; input{i}(j, 4)], input{i}(j, 5 : 7)');
+                            w = support_files.waypoint(input{i}(j, 1 : 3)', [NaN; NaN; input{i}(j, 4)], input{i}(j, 5 : 7)');
                         case 9
-                            w = support_files.waypoint(support_files.waypoint.Types_PositionRPYForce, ...
-                                input{i}(j, 1 : 3)', input{i}(j, 4 : 6)', input{i}(j, 7 : 9)');
+                            w = support_files.waypoint(input{i}(j, 1 : 3)', input{i}(j, 4 : 6)', input{i}(j, 7 : 9)');
                         otherwise
                             error('Error processing the input waypoints.');
                     end
