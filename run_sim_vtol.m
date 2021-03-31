27b3bc326ef0874031d81b3800089328811a116f
Azarakhsh Keipour <keipour@gmail.com>
2021-03-31 18:36:26 -0400

    Run Sim VTOL: Added angle of attack, side slip and airspeed plotting

--- run_sim_vtol.m
+++ run_sim_vtol.m
@@ -52,6 +52,7 @@ sim.SimulateTrajectory(traj, pos_thresh, rpy_thresh, force_thresh);
 
 graphics.PlotSignalsByName(3, {'pos', 'vel', 'accel', 'rpy', 'euler deriv', 'ang accel'}, false, true);
 graphics.PlotSignalsByName(2, {'servo', 'inward', 'sideward'}, false, true);
+graphics.PlotSignalsByName(2, {'alpha', 'beta', 'airspeed'});
 
 %% Animate the result
 
