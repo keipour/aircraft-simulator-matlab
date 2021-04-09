d7993f4b90cac54d3fc7b6842e9c69f66f1c8cac
Azarakhsh Keipour <keipour@gmail.com>
2021-04-09 09:18:52 -0400

    Robots: ODAR definition modified based on the paper

--- +robots/odar.m
+++ +robots/odar.m
@@ -1,10 +1,14 @@
+% From paper "ODAR: Aerial Manipulation Platform Enabling Omnidirectional
+% Wrench Generation"
+
 function m = odar(add_arm)
     if nargin < 1
         add_arm = false;
     end
 
     RotorPlacementAngles = zeros(1, 8);
-    RotorRotationDirections = [-1, 1, -1, 1, -1, 1, -1, 1];    
+    rot_dirs = [-1, 1, 1, 1];
+    RotorRotationDirections = [rot_dirs, -rot_dirs];
     RotorDihedralAngle = 0;
     RotorSidewardAngle = 0;
     RotorInwardAngle = 0;
@@ -12,33 +16,37 @@ function m = odar(add_arm)
     m = multirotor(RotorPlacementAngles, RotorRotationDirections);
     m.SetRotorAngles(RotorInwardAngle, RotorSidewardAngle, RotorDihedralAngle);
 
-    m.Rotors{1}.SetRotorAxis([0; 0; 1]);
-    m.Rotors{2}.SetRotorAxis([0; 1; 0]);
-    m.Rotors{3}.SetRotorAxis([0; 0; -1]);
-    m.Rotors{4}.SetRotorAxis([0; -1; 0]);
-    m.Rotors{5}.SetRotorAxis([0; 0; 1]);
-    m.Rotors{6}.SetRotorAxis([0; 1; 0]);
-    m.Rotors{7}.SetRotorAxis([0; 0; -1]);
-    m.Rotors{8}.SetRotorAxis([0; -1; 0]);
+    m.Rotors{1}.SetRotorAxis([0.68; 0.28; 0.68]);
+    m.Rotors{2}.SetRotorAxis([0.68; 0.28; -0.68]);
+    m.Rotors{3}.SetRotorAxis([0.68; -0.28; 0.68]);
+    m.Rotors{4}.SetRotorAxis([0.68; -0.28; -0.68]);
     
-    long_arm = 1; % in meters
-    short_arm = 0.3; % in meters
+    long_arm = 0.4; % in meters
+    short_arm = 0.24; % in meters
     front_end = [long_arm; 0; 0];
     back_end = [-long_arm; 0; 0];
     m.AddRod(back_end, front_end);
-    for i = 1 : 4
-        m.Rotors{i}.SetPosition(front_end - short_arm * m.Rotors{i}.R_BR(:, 3));
-        m.AddRod(front_end, front_end - short_arm * m.Rotors{i}.R_BR(:, 3));
-    end
-    for i = 5 : 8
-        m.Rotors{i}.SetPosition(back_end - short_arm * m.Rotors{i}.R_BR(:, 3));
-        m.AddRod(back_end, back_end - short_arm * m.Rotors{i}.R_BR(:, 3));
-    end    
 
-    MinimumRotorSpeed = -100; % Percentage of the minimum limit to make rotors bidirectional
-    for i = 1 : m.NumOfRotors
-        m.Rotors{i}.LowerSpeedPercentage = MinimumRotorSpeed;
+    short_arm_cos45 = short_arm * cosd(45);
+    m.Rotors{1}.SetPosition(front_end + [0; -short_arm_cos45; short_arm_cos45]);
+    m.Rotors{2}.SetPosition(front_end + [0; short_arm_cos45; short_arm_cos45]);
+    m.Rotors{3}.SetPosition(front_end + [0; -short_arm_cos45; -short_arm_cos45]);
+    m.Rotors{4}.SetPosition(front_end + [0; short_arm_cos45; -short_arm_cos45]);
+    
+    for i = 5 : 8
+        m.Rotors{i}.SetRotorAxis(m.Rotors{i - 4}.R_BR(:, 3));
+        m.Rotors{i}.SetPosition(-m.Rotors{i - 4}.Position);
+        m.AddRod(front_end, m.Rotors{i - 4}.Position);
+        m.AddRod(back_end, m.Rotors{i}.Position);
     end
+
+    % Make the rotors bi-directional and smaller
+    for i = 1 : m.NumOfRotors
+        m.Rotors{i}.LowerSpeedPercentage = -100; % Percentage of the minimum speed
+        m.Rotors{i}.Diameter = 10; % in inch (just for visualization)
+    end
+    
+    m.Mass = 2.6; % in Kg
     
     if add_arm
         m.AddEndEffector(arm);
